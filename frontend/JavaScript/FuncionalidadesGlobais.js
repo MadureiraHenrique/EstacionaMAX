a5b8d2240f0e0dd584662e2dc98d1c19522db9bd
Henrique Madureira <carloshenriquemadureira.santos@ucsal.edu.br>
2025-11-09 02:48:26 -0300

    Implementado telas de erro, pórem com a qualidade relativamente baixa e com alguns erros na leitura.

--- frontend/JavaScript/FuncionalidadesGlobais.js
+++ frontend/JavaScript/FuncionalidadesGlobais.js
@@ -1,28 +1,28 @@
-const menuItem = document.querySelectorAll('.item-menu');
-const conteinePrincipal = document.getElementById('conteiner-principal');
-const btnExp = document.querySelector('#btn-exp');
-const menuSide = document.querySelector('.menu-lateral');
-const iconeSair = document.getElementById('icone-sair');
+const menuItem = document.querySelectorAll(".item-menu");
+const conteinePrincipal = document.getElementById("conteiner-principal");
+const btnExp = document.querySelector("#btn-exp");
+const menuSide = document.querySelector(".menu-lateral");
+const iconeSair = document.getElementById("icone-sair");
 
 function selectLink() {
-    menuItem.forEach((item) => {
-        item.classList.remove('ativo');
-    });
+  menuItem.forEach((item) => {
+    item.classList.remove("ativo");
+  });
 
-    this.classList.add('ativo');
+  this.classList.add("ativo");
 }
 
 menuItem.forEach((item) => {
-    item.addEventListener('click', selectLink); 
+  item.addEventListener("click", selectLink);
 });
 
-btnExp.addEventListener('click', function() {
-    menuSide.classList.toggle('expandir');
-    conteinePrincipal.classList.toggle('ativar');
+btnExp.addEventListener("click", function () {
+  menuSide.classList.toggle("expandir");
+  conteinePrincipal.classList.toggle("ativar");
 });
 
-iconeSair.addEventListener('click', function() {
-    if (confirm('Deseja realmente sair?')) {
-        location.href = '../../HTML/Funcionario/Login.html';
-    }
+iconeSair.addEventListener("click", function () {
+  if (confirm("Deseja realmente sair?")) {
+    location.href = "../../HTML/HomePage.html";
+  }
 });
